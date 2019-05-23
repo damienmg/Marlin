71a1f2ed3c5bef8ae7eda2560875e769d819ab36
Damien Martin-Guillerez <dmg@nova.gs>
2019-05-23 20:17:23 +0200

    Set the number of microstep to 256
    This needs to be accompanied by the following to update the
    machine:
    
    ```
    M92 X1280 Y1280 Z6400 E1488  ; set the step per mm for each axis
    M913 X100 Y100 Z3 E30 ; set hybrid threshold in mm/s for each axis
    M500 ; Store the settings in the eeprom
    ```
    
    For me this settings is getting noiser than the 16 with interpolation
    so I am going back to 16 native microsteps.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1551,7 +1551,7 @@
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT     760  // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_MICROSTEPS  16  // 0..256
+    #define X_MICROSTEPS  256  // 0..256
     #define X_RSENSE     0.11
   #endif
 
@@ -1563,7 +1563,7 @@
 
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT     760
-    #define Y_MICROSTEPS  16
+    #define Y_MICROSTEPS  256
     #define Y_RSENSE     0.11
   #endif
 
@@ -1575,7 +1575,7 @@
 
   #if AXIS_IS_TMC(Z)
     #define Z_CURRENT     760
-    #define Z_MICROSTEPS  16
+    #define Z_MICROSTEPS  256
     #define Z_RSENSE     0.11
   #endif
 
@@ -1593,7 +1593,7 @@
 
   #if AXIS_IS_TMC(E0)
     #define E0_CURRENT    900
-    #define E0_MICROSTEPS 16
+    #define E0_MICROSTEPS 256
     #define E0_RSENSE    0.11
   #endif
 
