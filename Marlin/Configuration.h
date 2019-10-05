bbe56c59f8ca6416cf5ed810180f02b85aa8d49b
Damien Martin-Guillerez <dmg@nova.gs>
2019-10-05 19:31:36 +0200

    Update the offset for the BL-Touch with the new fanduct
    See https://www.thingiverse.com/thing:3751368

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -967,7 +967,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 44, 0, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 49, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
